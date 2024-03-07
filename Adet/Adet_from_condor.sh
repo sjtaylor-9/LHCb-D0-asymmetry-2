15886a3372f174079cad64f1c9889d9f9b918c86
Samuel James Taylor <sjtaylor@lxplus987.cern.ch>
2024-03-07 11:57:30 +0100

    Adapted to work with local Adet

--- Adet/Adet_from_condor.sh
+++ Adet/Adet_from_condor.sh
@@ -3,17 +3,24 @@
 # this file details what you would do at the command line, if you were to do everything locally on lxplus.
 # instead write the instructions for the batch system to do it:
 
-
+# Get the bin number from $6
+bin=$6
+# Check if bin number is less than 10
+if [ $bin -lt 10 ]; then
+    # If bin number is less than 10, prepend '0' to make it two digits
+    bin="0$bin"
+fi
+echo "Formatted input: $bin"
 
 # first do the reweighting.
 # you need to pass your input kpi data
 # the kpipi and kspi data are read from a location on /eos
 # so dont worry about that
-#--input /eos/lhcb/user/l/lseelan/Total/selected_data/${1}/${2:3:4}/both/up_data_16_130_clean.root:D02Kpi_Tuple/DecayTree \
+#--input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/${1}/1mil_${1:2:2}_${2:3:4}.root:D02Kpi_Tuple/DecayTree \
 python reweight_control_modes.py \
---year $1 --polarity $2 --n-estimators $3 --learning-rate $4 --max-depth $5 $6 \
---input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/${1}/1mil_${1}_${1:2:2}.root:D02Kpi_Tuple/DecayTree \
---output-directory /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/
+--year $1 --polarity $2 --n-estimators $3 --learning-rate $4 --max-depth $5 \
+--input /eos/lhcb/user/l/lseelan/Total/binned_data/${1:2:2}/local/both/${2:3}_${1:2:2}_70_bin${bin}.root:D02Kpi_Tuple/DecayTree \
+--output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/ --overwrite
 
 # this produces some new data files in the specified output directory
 # of kpipi and kspi with the generated weights (data.root and temp_**.root)
@@ -25,10 +32,10 @@ python reweight_control_modes.py \
 
 # next plot the results using the outputs from the previous command
 python plot_reweighted_control_modes.py \
---input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/data.root \
---kpipi-input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/temp_kpipi.root \
---kspi-input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/temp_kspi.root \
---output-directory /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/ --overwrite
+--input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/data.root \
+--kpipi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kpipi.root \
+--kspi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kspi.root \
+--output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/ --overwrite
 # # writes to the same location, doesn't actually overwrite anything
 # # as it only generates plots
 
@@ -37,13 +44,16 @@ python plot_reweighted_control_modes.py \
 # to reweight and fit the m(D+/-) distributions for both kpipi and kspi modes
 for mode in {kpipi,kspi}; do
   python fit_control_modes.py \
-  --input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/temp_${mode}.root \
+  --input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_${mode}.root \
   --control-mode ${mode} \
-  --output-directory /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/Adet/$1/$2/${6:2:5}/${mode}/ \
+  --output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/${mode}/ \
   --polarity $2
-# done
+done
 
 
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/data.root
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kpipi.root
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kspi.root
 
 
 # to submit jobs do:
