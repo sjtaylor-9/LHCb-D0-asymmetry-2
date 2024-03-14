59762b2fcfe0eff0175b0135c8f8f652921c49a9
Samuel James Taylor <sjtaylor@lxplus950.cern.ch>
2024-03-14 23:40:24 +0100

    Local Adet works

--- Adet/Adet_from_condor.sh
+++ Adet/Adet_from_condor.sh
@@ -6,11 +6,11 @@
 # Get the bin number from $6
 bin=$6
 # Check if bin number is less than 10
-if [ $bin -lt 10 ]; then
-    # If bin number is less than 10, prepend '0' to make it two digits
-    bin="0$bin"
-fi
-echo "Formatted input: $bin"
+# if [ $bin -lt 10 ]; then
+#     # If bin number is less than 10, prepend '0' to make it two digits
+#     bin="0$bin"
+# fi
+# echo "Formatted input: $bin"
 
 # first do the reweighting.
 # you need to pass your input kpi data
@@ -19,8 +19,8 @@ echo "Formatted input: $bin"
 #--input /afs/cern.ch/user/s/sjtaylor/WorkSpace/D0_production_asymmetry_Sem2/LHCb_D0_asymmetry_2/${1}/1mil_${1:2:2}_${2:3:4}.root:D02Kpi_Tuple/DecayTree \
 python reweight_control_modes.py \
 --year $1 --polarity $2 --n-estimators $3 --learning-rate $4 --max-depth $5 \
---input /eos/lhcb/user/l/lseelan/Total/binned_data/${1:2:2}/local/both/${2:3}_${1:2:2}_70_bin${bin}.root:D02Kpi_Tuple/DecayTree \
---output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/ --overwrite
+--input /eos/lhcb/user/l/lseelan/Total/binned_data/${1:2:2}/pT/both/${2:3}_${1:2:2}_70_bin${bin}.root:D02Kpi_Tuple/DecayTree \
+--output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/ --overwrite
 
 # this produces some new data files in the specified output directory
 # of kpipi and kspi with the generated weights (data.root and temp_**.root)
@@ -32,10 +32,10 @@ python reweight_control_modes.py \
 
 # next plot the results using the outputs from the previous command
 python plot_reweighted_control_modes.py \
---input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/data.root \
---kpipi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kpipi.root \
---kspi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kspi.root \
---output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/ --overwrite
+--input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/data.root \
+--kpipi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/temp_kpipi.root \
+--kspi-input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/temp_kspi.root \
+--output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/ --overwrite
 # # writes to the same location, doesn't actually overwrite anything
 # # as it only generates plots
 
@@ -44,16 +44,16 @@ python plot_reweighted_control_modes.py \
 # to reweight and fit the m(D+/-) distributions for both kpipi and kspi modes
 for mode in {kpipi,kspi}; do
   python fit_control_modes.py \
-  --input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_${mode}.root \
+  --input /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/temp_${mode}.root \
   --control-mode ${mode} \
-  --output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/${mode}/ \
+  --output-directory /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/${mode}/ --overwrite \
   --polarity $2
 done
 
 
-rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/data.root
-rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kpipi.root
-rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/local/$1/$2/${bin}/temp_kspi.root
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/data.root
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/temp_kpipi.root
+rm /eos/lhcb/user/s/sjtaylor/D0_asymmetry/Adet/pT/$1/$2/${bin}/temp_kspi.root
 
 
 # to submit jobs do:
