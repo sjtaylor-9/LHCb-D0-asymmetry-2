5e398bbf1e238a3e5de9cb03ee5b89020c814c66
Samuel James Taylor <sjtaylor@lxplus959.cern.ch>
2024-03-25 20:59:25 +0100

    Removed

--- main_binning.sh
+++ main_binning.sh
@@ -4,10 +4,12 @@ size=$3
 
 mkdir $directory"/binned_data"
 mkdir $directory"/binned_data/binning_scheme"
-mkdir $directory"/binned_data"/$year
-mkdir $directory"/binned_data"/$year/"local"
-mkdir $directory"/binned_data"/$year/"pT"
-mkdir $directory"/binned_data"/$year/"eta"
+mkdir $directory"/binned_data"
+mkdir $directory"/binned_data/local"
+mkdir $directory"/binned_data/pT"
+mkdir $directory"/binned_data/eta"
+
+# These directories are made in the eos, which were used for Adet. They are not needed unless redoing Adet.
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/$year
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/$year/local
@@ -16,31 +18,36 @@ mkdir $directory"/binned_data"/$year/"eta"
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/$year/local/both
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/$year/pT/both
 # mkdir /eos/lhcb/user/l/lseelan/Total/binned_data/$year/eta/both
+
 # Size 10: lowest. Size 20: twice as big and includes data from Size 10
 
-# python create_binning_scheme.py --year $year --size $size --path $directory"/binned_data/binning_scheme" --input "/eos/lhcb/user/l/lseelan/Total/selected_data"
+python create_binning_scheme.py \
+    --year $year \
+    --size $size \
+    --path $directory"/binned_data/binning_scheme" \
+    --input "/eos/lhcb/user/l/lseelan/Total/selected_data"
 
-# for meson in D0 D0bar; do 
-#     for polar in up down; do    
-#         python apply_binning_scheme.py \
-#             --year $year \
-#             --size $size \
-#             --meson $meson \
-#             --polarity $polar \
-#             --path $directory"/binned_data/"$year \
-#             --input "/eos/lhcb/user/l/lseelan/Total/selected_data" \
-#             --bin_path $directory"/binned_data/binning_scheme"
-#         python plot_phase_space.py \
-#             --year $year \
-#             --size $size \
-#             --meson $meson \
-#             --polarity $polar \
-#             --path $directory"/binned_data/binning_scheme" \
-#             --input "/eos/lhcb/user/l/lseelan/Total/selected_data" \
-#             --bin_path $directory"/binned_data/binning_scheme"
-#         echo "Ploted 2D graph"
-#     done
-# done
+for meson in D0 D0bar; do 
+    for polar in up down; do    
+        python apply_binning_scheme.py \
+            --year $year \
+            --size $size \
+            --meson $meson \
+            --polarity $polar \
+            --path $directory"/binned_data/" \
+            --input "/eos/lhcb/user/l/lseelan/Total/selected_data" \
+            --bin_path $directory"/binned_data/binning_scheme"
+        python plot_phase_space.py \
+            --year $year \
+            --size $size \
+            --meson $meson \
+            --polarity $polar \
+            --path $directory"/binned_data/binning_scheme" \
+            --input "/eos/lhcb/user/l/lseelan/Total/selected_data" \
+            --bin_path $directory"/binned_data/binning_scheme"
+        echo "Ploted 2D graph"
+    done
+done
 
 echo "The data has been binned"
 echo
@@ -52,7 +59,7 @@ for polar in up down; do
         --size $size \
         --meson 'both' \
         --polarity $polar \
-        --path $directory"/binned_data"/$year \
+        --path $directory"/binned_data" \
         --input "/eos/lhcb/user/l/lseelan/Total/selected_data" \
         --bin_path $directory"/binned_data/binning_scheme"
 done
