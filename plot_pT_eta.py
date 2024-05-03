9d4458c1048caac3e51a28b92edf6edd0718c1ab
Samuel James Taylor <sjtaylor@lxplus9111.cern.ch>
2024-05-04 01:29:00 +0200

    Added global Adet capabilities

--- plot_pT_eta.py
+++ plot_pT_eta.py
@@ -130,20 +130,32 @@ def getCumY(yy, yKS):
     return cKS
 # - - - - - - - MAIN BODY - - - - - - - #
 args = parse_arguments()
-
+scheme = args.scheme
 unbinned_asymm, unbinned_asymm_error = read_asymmetry_values()
 
 asymmetry = np.array([])
 asymmetry_error = np.array([])
 for j in range(0,10):
     bin_num = str(j)
-    with open(f'{args.asymm_path}/asymmetries_{args.year}_{args.size}_bin{bin_num}.txt') as f:
+    with open(f'{args.asymm_path}/asymmetries_{args.year}_{args.size}_bin{bin_num}_detection_scheme_{scheme}.txt') as f:
         lines = f.readlines()
         A_prod = float(lines[0])
         A_prod_err = float(lines[1])
     f.close()
     asymmetry = np.append(asymmetry, A_prod)
     asymmetry_error = np.append(asymmetry_error, A_prod_err)
+
+asymmetry_global_detection = np.array([])
+asymmetry_error_global_detection = np.array([])
+for j in range(0,10):
+    bin_num = str(j)
+    with open(f'{args.asymm_path}/asymmetries_{args.year}_{args.size}_bin{bin_num}_detection_scheme_global.txt') as f:
+        lines = f.readlines()
+        A_prod = float(lines[0])
+        A_prod_err = float(lines[1])
+    f.close()
+    asymmetry_global_detection = np.append(asymmetry_global_detection, A_prod)
+    asymmetry_error_global_detection = np.append(asymmetry_error_global_detection, A_prod_err)
 
 simulated_asymmetry = np.array([])
 simulated_asymmetry_error = np.array([])
@@ -194,7 +206,8 @@ ax1.tick_params(axis='both', which='both', labelsize=30)
 line2 = ax1.axhline(unbinned_asymm, color='red', linestyle='solid', linewidth=5)
 fill2 = ax1.axhspan(unbinned_asymm-unbinned_asymm_error, unbinned_asymm+unbinned_asymm_error, color='red', alpha=0.35, lw=0)
 
-Data = ax1.errorbar(x_value, asymmetry, yerr=asymmetry_error,xerr=x_value_error, fmt='o', capsize=5, color = 'black', label = 'Data')
+Data = ax1.errorbar(x_value, asymmetry, yerr=asymmetry_error,xerr=x_value_error, fmt='o', capsize=5, color = 'black', label = r'Data (Inc. local $A_{\mathrm{det}}$)', alpha=0.75)
+Data2 = ax1.errorbar(x_value, asymmetry_global_detection, yerr=asymmetry_error_global_detection, xerr=x_value_error, fmt='o', capsize=5, color = 'blue', label = r'Data (Inc. global $A_{\mathrm{det}}$)', alpha=0.6)
 
 # Fill between simulated error bars
 for i in range(len(x_value)):
@@ -208,9 +221,9 @@ extra = Rectangle((0, 0), 1, 1, fc="green", fill=True, edgecolor='none', linewid
 # Fit = ax.axhline(values[0], color='purple', linestyle=':', linewidth=5)
 
 if args.scheme == 'pT':
-    ax1.legend([(line2,fill2),Data, extra],[r'Bin integrated result','Data','Pythia'], loc='upper left')
+    ax1.legend([(line2,fill2),Data, Data2, extra],[r'Bin integrated result', r'Data (Inc. local $A_{\mathrm{det}}$)', r'Data (Inc. global $A_{\mathrm{det}}$)', 'Pythia'], loc='upper left')
 elif args.scheme == 'eta':
-    ax1.legend([(line2,fill2),Data, extra],[r'Bin integrated result','Data','Pythia'], loc='best')
+    ax1.legend([(line2,fill2),Data, Data2, extra],[r'Bin integrated result', r'Data (Inc. local $A_{\mathrm{det}}$)', r'Data (Inc. global $A_{\mathrm{det}}$)','Pythia'], loc='best')
 
 if args.scheme == 'pT':
     ax1.set_xlabel(r'$p_{T}$ [GeV$c^{-1}$]', fontsize = 50)
@@ -249,7 +262,10 @@ ax[1].plot(yKS,dKS, color = "green", label = r'$\Delta$ CDF')
 ax[0].set_yticks(np.arange(0, 1 + 0.2, 0.25))
 ax[1].set_yticks(np.arange(0, max(dKS) + 0.2, 0.25))
 # Add more ticks on the x-axis
-ax[0].set_xticks(np.arange(round(min_value_asymmetry,1)-0.1, round(max_value_asymmetry,1) + 0.1, 0.2))
+if max_value_asymmetry > 1.5 or min_value_asymmetry < 1.5:
+        ax[0].set_xticks(np.arange(round(min_value_asymmetry,1)-0.1, round(max_value_asymmetry,1) + 0.1, 0.4))
+else:
+    ax[0].set_xticks(np.arange(round(min_value_asymmetry,1)-0.1, round(max_value_asymmetry,1) + 0.1, 0.2))
 ax[1].set_xticks(ax[0].get_xticks())
 ax[0].set_ylabel(r'CDF',fontsize=50)
 ax[1].set_xlabel(r'Production Asymmetry [%]',fontsize=50)
